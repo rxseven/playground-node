a22e3ea596c1dba93a59ea661c14804029ec8afe
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-13 21:23:50 +0700

    Add Express app with basic configuration

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+// Module dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('../db/mongoose');
+const { Todo } = require('../models/Todo');
+const { User } = require('../models/User');
+
+// Create Express server
+const app = express();
+
+// Body parsing
+app.use(bodyParser.json());
+
+// Bind and listen for connections on the specified host and port
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is listening on port 5000');
+});
+
+// Module exports
+module.exports = app;
