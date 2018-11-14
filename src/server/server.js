e14eb1b7b21d0273540c02a9e73bbb41be22e8c6
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-14 20:58:22 +0700

    Add POST /todo endpoint for adding todo

--- src/server/server.js
+++ src/server/server.js
@@ -12,6 +12,25 @@ const app = express();
 // Body parsing
 app.use(bodyParser.json());
 
+// Add todo
+app.post('/todos', (req, res) => {
+  // Create new document
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  // Save the document
+  todo.save().then((document) => {
+    res
+      .status(201)
+      .send(document);
+  }, (error) => {
+    res
+      .status(400)
+      .send(error);
+  })
+});
+
 // Bind and listen for connections on the specified host and port
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is listening on port 5000');
