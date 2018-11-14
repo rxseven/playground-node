fff899be7f948e0ca61449d2e3cb24c883b56b91
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-14 21:07:09 +0700

    Add tests for POST /todos endpoint

--- src/server/tests/server.test.js
+++ src/server/tests/server.test.js
@@ -18,4 +18,47 @@ beforeEach(function(done) {
 describe('POST /todos', function() {
   // Disable timeout for test suite
   this.timeout(0);
+
+  it('should create a new todo', (done) => {
+    // Variables
+    const text = 'Test todo text';
+
+    // Assertions
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should not create todo with invalid body data', (done) => {
+    // Assertions
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
 });
