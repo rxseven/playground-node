29891e015b764ad51d161f9b8f294da31ff6f874
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-30 10:49:35 +0700

    Add additional properties to User model

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,5 +1,6 @@
 // Module dependencies
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 // Model
 const User = mongoose.model('User', {
@@ -7,8 +8,30 @@ const User = mongoose.model('User', {
     minlength: 1,
     required: true,
     trim: true,
+    type: String,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    minlength: 6,
+    require: true,
     type: String
-  }
+  },
+  tokens: [
+    {
+      access: {
+        require: true,
+        type: String
+      },
+      token: {
+        require: true,
+        type: String
+      }
+    }
+  ]
 });
 
 // Module exports
